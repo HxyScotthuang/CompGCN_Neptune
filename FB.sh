13ce812da64b6c937710121fb2530eb9c51f5bd6
Xingyue Huang <49787534+HxyScotthuang@users.noreply.github.com>
2023-03-15 17:13:23 +0000

    Create FB.sh

---
+++ FB.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --time=11:59:59
+#SBATCH --job-name=compgcn
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=short
+#SBATCH --gres=gpu:v100:1 
+
+module purge
+module load Anaconda3
+source activate $DATA/myenv
+
+python run.py -score_func distmult -opn mult -gcn_layer 3 -gpu 0 -data FB15k-237 -name FB 
